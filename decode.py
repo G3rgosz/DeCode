e090fd6cf451fd705ca5d4db24d6bdf99d278c49
Gergosz-2000 <gergoszivak@gmail.com>
2020-10-14 11:14:20 +0200

    mukszik

--- decode.py
+++ decode.py
@@ -1,11 +1,31 @@
+Dekod="mnbvcxyíűáélkjhgfdsaóüöúőpoiuztrewq"
 print("Válassz az alábbi lehetőségek közül:")
-print("1 = DeKód    2 = Kódol")
+print("1 = Kódol    2 = DeKódol")
 szam = int(input("Kérem írja be a választott művelet számát: "))
 if (szam == 1):
 	szoveg = str(input("Kódolandó szöveg: "))
-	ls = [len(szöveg)]
+	def kodolt(szoveg):
+		szoveg = szoveg.replace("a","2")
+		szoveg = szoveg.replace("e","3")
+		szoveg = szoveg.replace("o","4")
+		szoveg = szoveg.replace("u","5")
+		ujSzoveg = ""
+		for x in range(len(szoveg)-1,-1,-1):
+			ujSzoveg = ujSzoveg + szoveg[x]
+		return ujSzoveg
+	print(kodolt(szoveg))
 elif (szam == 2):
 	szoveg = str(input("DeKódolandó szöveg: "))
+	def kodolt(szoveg):
+		szoveg = szoveg.replace("2","a")
+		szoveg = szoveg.replace("3","e")
+		szoveg = szoveg.replace("4","o")
+		szoveg = szoveg.replace("5","u")
+		ujSzoveg = ""
+		for x in range(len(szoveg)-1,-1,-1):
+			ujSzoveg = ujSzoveg + szoveg[x]
+		return ujSzoveg
+	print(kodolt(szoveg))
 else:
 	print("nincs ilyen lehetőség")
 	
@@ -23,9 +43,7 @@ else:
 
 
 	
-szoveg=""
-Kod="qwertzuiopőúöüóasdfghjkléáűíyxcvbnm"
-Dekod="mnbvcxyíűáélkjhgfdsaóüöúőpoiuztrewq"
+
 
 
 

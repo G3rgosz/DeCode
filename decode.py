b4a1698f41343ed4b62ccb75ae6f8c257158aee0
Gergosz-2000 <gergoszivak@gmail.com>
2020-10-14 10:14:55 +0200

    dekód 0.1

--- decode.py
+++ decode.py
@@ -2,9 +2,9 @@ print("Válassz az alábbi lehetőségek közül:")
 print("1 = DeKód    2 = Kódol")
 szam = int(input("Kérem írja be a választott művelet számát: "))
 if (szam == 1):
-
+	szoveg = str(input("Kódolandó szöveg: "))
 elif (szam == 2):
-	
+	szoveg = str(input("DeKódolandó szöveg: "))
 else:
 	print("nincs ilyen lehetőség")
 

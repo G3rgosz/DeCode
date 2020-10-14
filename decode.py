9062e8ea4300f268321b742db395c293c541a574
Gergosz-2000 <gergoszivak@gmail.com>
2020-10-14 12:09:47 +0200

    2.0

--- decode.py
+++ decode.py
@@ -4,28 +4,40 @@ print("1 = Kódol    2 = DeKódol")
 szam = int(input("Kérem írja be a választott művelet számát: "))
 if (szam == 1):
 	szoveg = str(input("Kódolandó szöveg: "))
-	def kodolt(szoveg):
-		szoveg = szoveg.replace("a","2")
-		szoveg = szoveg.replace("e","3")
-		szoveg = szoveg.replace("o","4")
-		szoveg = szoveg.replace("u","5")
+	v=""
+	karakterek="QWERTZUIOPŐÚÖÜÓASDFGHJKLÉÁŰÍYXCVBNMqwerztuiopőúasdfghjkléáűíyxcvbnmöüóÖÜÓ0123456789"
+	kodolo="9876543210ÓÜÖóüömnbvcxyíűáélkjhgfds2úőp4i5tzr3wqMNBVCXYÍŰÁÉLKJHGFDSAÓÜÖÚŐPOIUZTREWQ"
+	for x in range (0, len(szoveg)):
+		i=karakterek.index(szoveg[x])
+		v+=kodolo[i]
+	def kodolt(v):
+		v = v.replace("a","2")
+		v = v.replace("e","3")
+		v = v.replace("o","4")
+		v = v.replace("u","5")
 		ujSzoveg = ""
-		for x in range(len(szoveg)-1,-1,-1):
-			ujSzoveg = ujSzoveg + szoveg[x]
+		for x in range(len(v)-1,-1,-1):
+			ujSzoveg = ujSzoveg + v[x]
 		return ujSzoveg
-	print(kodolt(szoveg))
+	print(kodolt(v))
 elif (szam == 2):
 	szoveg = str(input("DeKódolandó szöveg: "))
-	def kodolt(szoveg):
-		szoveg = szoveg.replace("2","a")
-		szoveg = szoveg.replace("3","e")
-		szoveg = szoveg.replace("4","o")
-		szoveg = szoveg.replace("5","u")
+	v = ""
+	karakterek="QWERTZUIOPŐÚÖÜÓASDFGHJKLÉÁŰÍYXCVBNMqwerztuiopőúasdfghjkléáűíyxcvbnmöüóÖÜÓ0123456789"
+	kodolo="9876543210ÓÜÖóüömnbvcxyíűáélkjhgfds2úőp4i5tzr3wqMNBVCXYÍŰÁÉLKJHGFDSAÓÜÖÚŐPOIUZTREWQ"
+	for x in range (0, len(szoveg)):
+		i=kodolo.index(szoveg[x])
+		v+=karakterek[i]
+	def kodolt(v):
+		v = v.replace("2","a")
+		v = v.replace("3","e")
+		v = v.replace("4","o")
+		v = v.replace("5","u")
 		ujSzoveg = ""
-		for x in range(len(szoveg)-1,-1,-1):
-			ujSzoveg = ujSzoveg + szoveg[x]
+		for x in range(len(v)-1,-1,-1):
+			ujSzoveg = ujSzoveg + v[x]
 		return ujSzoveg
-	print(kodolt(szoveg))
+	print(kodolt(v))
 else:
 	print("nincs ilyen lehetőség")
 	
--- decode2.py
+++ decode2.py
@@ -1,7 +1,17 @@
 szoveg="proba"
+"""
 print(ord('A'))
+print(ord('a'))
+print()
 
+kodolt=""
 
 for x in range (0, len(szoveg)):
-	print (ord(szöveg[x]*3+13)
-	
+	kodolt+=str(ord(szoveg[x])*3+13)"""
+karakterek="QWERTZUIOPŐÚÖÜÓASDFGHJKLÉÁŰÍYXCVBNMqwerztuiopőúasdfghjkléáűíyxcvbnmöüóÖÜÓ0123456789"
+kodolo="9876543210ÓÜÖóüömnbvcxyíűáélkjhgfds2úőp4i5tzr3wqMNBVCXYÍŰÁÉLKJHGFDSAÓÜÖÚŐPOIUZTREWQ"
+kodolt=""
+for x in range (0, len(szoveg)):
+	i=karakterek.index(szoveg[x])
+	kodolt+=kodolo[i]
+print(kodolt)
